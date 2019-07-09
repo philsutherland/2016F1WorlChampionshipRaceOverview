2c5430098638e60c627384e805d4f045d6625e64
Phil Sutherland <psuth026@uottawa.ca>
2019-07-09 15:32:52 -0400

    Fixed formatting glitch

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ points_per_position = {"1": 25, "2": 18, "3": 15, "4": 12,
 Rosberg_Hamilton_score = [["AUS", "1", "2"], ["BHR", "1", "3"], ["CHN", "1", "7"], ["RUS", "1", "2"], ["ESP", "R", "R"], ["MON", "7", "1"], ["CAN", "5", "1"], ["EUR", "1", "5"], ["AUT", "4", "1"], [
     "GBR", "3", "1"], ["HUN", "2", "1"], ["GER", "4", "1"], ["BEL", "1", "3"], ["ITA", "1", "2"], ["SIN", "1", "3"], ["MAL", "3", "R"], ["JPN", "1", "3"], ["USA", "2", "1"], ["MEX", "2", "1"], ["BRA", "2", "1"], ["ABU", "2", "1"]]
 
+# Add some blank spaces before the start
+for x in range(5):
+    print("")
+
 print("------------------------------------------------------------------------------------")
-print("{:^84}".format("Welcome to the 2016 F1 Championship"))
+print("{:^84}".format("Overview of the 2016 F1 World Championship"))
 print("------------------------------------------------------------------------------------")
 print("────────────────────────────────────────────────────────────────────────────────────")
 print("| Grand Prix Location | Nico Rosberg | Lewis Hamilton | Point Difference (Rosberg) |")
